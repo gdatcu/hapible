5cc43cc5360815e4ef84224cb2b937d4f0cc485c
George Datcu <143513063+gdatcu@users.noreply.github.com>
2025-07-25 09:49:10 +0300

    Update AuthController.php

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Folosim calea corectă, cu forward slashes
 require_once __DIR__ . '/../../config/config.php';
 
